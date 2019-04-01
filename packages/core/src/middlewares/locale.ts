34ae73740498c67a7a132e6154e7f877c485380f
jeanfortheweb <jeanfortheweb@gmail.com>
2019-04-01 10:56:12 +0200

    Docs: Add inline docs for the core package

--- packages/core/src/middlewares/locale.ts
+++ packages/core/src/middlewares/locale.ts
@@ -1,9 +1,20 @@
 import { Middleware } from '..';
 
+/**
+ * Translator options used by many different translator middlewares.
+ */
 export interface LocaleOptions {
+  /**
+   * Sets the locale to used for translation.
+   */
   locale?: string;
 }
 
+/**
+ * Sets a default locale to use when no other is given.
+ *
+ * @param locale Locale to use.
+ */
 export function withDefaultLocale(locale: string): Middleware<LocaleOptions> {
   return next => (id, options) => {
     return next(id, {
@@ -13,13 +24,28 @@ export function withDefaultLocale(locale: string): Middleware<LocaleOptions> {
   };
 }
 
-export interface FallbackLocaleOptions extends LocaleOptions {
+/**
+ * Translator options for `withFallbackLocale`.
+ *
+ * @see withFallbackLocale
+ */
+export interface FallbackLocaleOptions {
+  /**
+   * Fallback locale to use when translation using the current locale resulted
+   * in undefined.
+   */
   fallback?: string;
 }
 
+/**
+ * Sets the locale to use when translation using the current locale has failed.
+ * The fallback locale can also be set as translator option, which then has priority.
+ *
+ * @param locale Locale to fallback to.
+ */
 export function withFallbackLocale(
   locale: string,
-): Middleware<FallbackLocaleOptions> {
+): Middleware<FallbackLocaleOptions & LocaleOptions> {
   return next => (id, options) => {
     const translated = next(id, options);
 
--- packages/core/src/middlewares/utility.ts
+++ packages/core/src/middlewares/utility.ts
@@ -1,6 +1,31 @@
 import { LocaleOptions } from './locale';
 import { Middleware } from '..';
 
+/**
+ * Enables logging of failed translations.
+ * Failed translations are logged using `console.warn`. This behavior can be changed by passing a
+ * custom logger function, which will receive the arguments which has been passed to the translator
+ * function.
+ *
+ * @example
+ * ```
+ * const _ = createTranslator(
+ *    withLogger()
+ * );
+ *
+ * _('missing.id', { locale: 'en' }); // Will log "Missing translation for 'missing.id' on locale 'en'
+ * ```
+ *
+ * @example
+ * ```
+ * const _ = createTranslator(
+ *    withLogger((id, options) => { console.error(id) })
+ * );
+ *
+ * _('missing.id', { locale: 'en' }); // Will log "missing.id"
+ * ```
+ * @param logger Logger function to use.
+ */
 export function withLogger(
   logger?: (id: string, options: LocaleOptions) => void,
 ): Middleware<LocaleOptions> {
@@ -21,6 +46,21 @@ export function withLogger(
   };
 }
 
+/**
+ * Enables fallback translations using the given fallback function. This function should return a fallback
+ * translation string based on the arguments which has been passed to the translator function.
+ *
+ * @example
+ * ```
+ * const _ = createTranslator(
+ *    withFallbackTranslation((id) => `Unable to translate ${id}`)
+ * );
+ *
+ * _('missing.id'); // === 'Unable to translate missing.id"
+ * ```
+ *
+ * @param translate Fallback translation function.
+ */
 export function withFallbackTranslation(
   translate: (id: string, options: LocaleOptions) => string,
 ): Middleware<LocaleOptions> {
